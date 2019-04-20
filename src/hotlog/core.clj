a39852d4fd0a77cc05ecb127da17ad62cb403a37
Danny Guinther <dannyguinther@gmail.com>
2019-04-19 21:28:01 -0400

    Add helpers for building file and console loggers

--- src/hotlog/core.clj
+++ src/hotlog/core.clj
@@ -1,5 +1,24 @@
 (ns hotlog.core
-  (:import [org.slf4j Logger]))
+  (:refer-clojure :exclude [name])
+  (:import [ch.qos.logback.classic Level]
+           [ch.qos.logback.classic.encoder PatternLayoutEncoder]
+           [ch.qos.logback.classic.filter ThresholdFilter]
+           [ch.qos.logback.core ConsoleAppender FileAppender]
+           [org.slf4j Logger LoggerFactory]))
+
+(def logger-levels
+  "Mapping from keyword to logback level"
+  {:debug Level/DEBUG
+   :error Level/ERROR
+   :info Level/INFO
+   :trace Level/TRACE
+   :warn Level/WARN})
+
+(def threshold-filter-levels
+  "Mapping from keyword to logback level"
+  (->> logger-levels
+       (map (fn [[kw level]] [kw (str level)]))
+       (into {})))
 
 (def noop-logger
   "Returns a new logger instance that does nothing, but implements the required interface."
@@ -15,3 +34,75 @@
     (isWarnEnabled [this] false)
     (trace [this _] nil)
     (warn [this _] nil)))
+
+(defn simple-encoder-factory [{:keys [context start?]
+                               :or {start? true}}]
+  (let [encoder (doto (PatternLayoutEncoder.)
+                      (.setContext context)
+                      (.setPattern "%date %level %logger [%thread]: %msg%n"))]
+    (when start?
+      (.start encoder))
+    encoder))
+
+(defn threshold-filter [{:keys [level start?]
+                         :or {start? true}}]
+  (let [filter-level (if (keyword? level)
+                       (level threshold-filter-levels)
+                       level)
+        threshold-filter* (doto (ThresholdFilter.)
+                                (.setLevel filter-level))]
+    (when start?
+      (.start threshold-filter*))
+    threshold-filter*))
+
+(defmulti build-appender (fn [config] (:type config)))
+
+(defmethod build-appender :console [{:keys [context encoder-factory level name start?]
+                                     :or {encoder-factory simple-encoder-factory
+                                          start? true}}]
+  (let [threshold-filter* (threshold-filter {:level level :start? start?})
+        appender (doto (ConsoleAppender.)
+                       (.setContext context)
+                       (.addFilter threshold-filter*))
+        encoder (encoder-factory {:context context :start? start?})]
+    (.setEncoder appender encoder)
+    (when name
+      (.setName appender name))
+    (when start?
+      (.start appender))
+    appender))
+
+(defmethod build-appender :file [{:keys [context encoder-factory level name path start?]
+                                  :or {encoder-factory simple-encoder-factory
+                                       start? true}}]
+  (let [threshold-filter* (threshold-filter {:level level :start? start?})
+        appender (doto (FileAppender.)
+                   (.setFile path)
+                   (.setContext context)
+                   (.addFilter threshold-filter*))
+        encoder (encoder-factory {:context context :start? start?})]
+    (.setEncoder appender encoder)
+    (when name
+      (.setName appender name))
+    (when start?
+      (.start appender))
+    appender))
+
+(defn build-logger [{:keys [additive? appenders context detach-and-stop-all-appenders? level name]
+                     :or {additive? false
+                          context (LoggerFactory/getILoggerFactory)
+                          detach-and-stop-all-appenders? true
+                          level :info}}]
+  (let [log-level (cond
+                   (keyword? level) (level logger-levels)
+                   (nil? level) (:info logger-levels)
+                   :else level)
+        logger (doto (LoggerFactory/getLogger name)
+                     (.setAdditive (or additive? false))
+                     (.setLevel log-level))]
+    (when detach-and-stop-all-appenders?
+      (.detachAndStopAllAppenders logger))
+    (doseq [appender-config appenders]
+      (let [appender (build-appender (assoc appender-config :context context))]
+        (.addAppender logger appender)))
+    logger))
--- test/hotlog/core_test.clj
+++ test/hotlog/core_test.clj
@@ -1,6 +1,66 @@
 (ns hotlog.core-test
   (:require [clojure.test :refer :all]
-            [hotlog.core :as subject]))
+            [hotlog.core :as subject])
+  (:import [ch.qos.logback.classic Level LoggerContext]
+           [ch.qos.logback.classic.spi LoggingEvent]
+           [ch.qos.logback.core ConsoleAppender FileAppender]
+           [ch.qos.logback.core.spi FilterReply]
+           [org.slf4j Logger LoggerFactory]))
+
+(def trace-event
+  (doto (LoggingEvent.)
+        (.setLevel Level/TRACE)))
+
+(def debug-event
+  (doto (LoggingEvent.)
+        (.setLevel Level/DEBUG)))
+
+(def info-event
+  (doto (LoggingEvent.)
+        (.setLevel Level/INFO)))
+
+(def warn-event
+  (doto (LoggingEvent.)
+        (.setLevel Level/WARN)))
+
+(def error-event
+  (doto (LoggingEvent.)
+        (.setLevel Level/ERROR)))
+
+(defn logger-context []
+  (LoggerFactory/getILoggerFactory))
+
+(deftest logger-levels
+  (testing ":debug"
+    (is (= Level/DEBUG (:debug subject/logger-levels))))
+
+  (testing ":error"
+    (is (= Level/ERROR (:error subject/logger-levels))))
+
+  (testing ":info"
+    (is (= Level/INFO (:info subject/logger-levels))))
+
+  (testing ":trace"
+    (is (= Level/TRACE (:trace subject/logger-levels))))
+
+  (testing ":warn"
+    (is (= Level/WARN (:warn subject/logger-levels)))))
+
+(deftest threshold-filter-levels
+  (testing ":debug"
+    (is (= "DEBUG" (:debug subject/threshold-filter-levels))))
+
+  (testing ":error"
+    (is (= "ERROR" (:error subject/threshold-filter-levels))))
+
+  (testing ":info"
+    (is (= "INFO" (:info subject/threshold-filter-levels))))
+
+  (testing ":trace"
+    (is (= "TRACE" (:trace subject/threshold-filter-levels))))
+
+  (testing ":warn"
+    (is (= "WARN" (:warn subject/threshold-filter-levels)))))
 
 (deftest noop-logger
   (testing "implements debug taking one argument and returning nil"
@@ -35,3 +95,299 @@
 
   (testing "implements warn taking one argument and returning nil"
     (is (nil? (.warn subject/noop-logger "")))))
+
+(deftest threshold-filter
+  (testing "filter configured with specified level"
+    (let [threshold-filter* (subject/threshold-filter {:level :info})]
+      (is (= FilterReply/DENY (.decide threshold-filter* trace-event)))
+      (is (= FilterReply/DENY (.decide threshold-filter* debug-event)))
+      (is (= FilterReply/NEUTRAL (.decide threshold-filter* info-event)))
+      (is (= FilterReply/NEUTRAL (.decide threshold-filter* warn-event)))
+      (is (= FilterReply/NEUTRAL (.decide threshold-filter* error-event)))))
+
+  (testing "filter is started by default"
+    (let [threshold-filter* (subject/threshold-filter {:level :info})]
+      (is (= true (.isStarted threshold-filter*)))))
+
+  (testing "filter is not started when :start? is given as false"
+    (let [threshold-filter* (subject/threshold-filter {:level :info :start? false})]
+      (is (= false (.isStarted threshold-filter*))))))
+
+(deftest simple-encoder-factory
+  (testing "encoder is configured with given context"
+    (let [context (logger-context)
+          encoder (subject/simple-encoder-factory {:context context})]
+      (is (= context (.getContext encoder)))))
+
+  ;; currently the encoder always returns false when encoder.isStarted is called. Uncomment this
+  ;; test if/when this has been fixed and logback has been updated.
+  ;; https://github.com/qos-ch/logback/pull/455
+  (comment
+   (testing "encoder is started by default"
+     (let [encoder (subject/simple-encoder-factory {:context (logger-context)})]
+       (Thread/sleep 1000)
+       (is (= true (.isStarted encoder))))))
+
+  ;; currently encoder.isStarted always returns false. See above.
+  (testing "encoder is not started when false is given for start?"
+    (let [encoder (subject/simple-encoder-factory {:context (logger-context)
+                                                   :start? false})]
+      (is (= false (.isStarted encoder)))))
+
+  (testing "encoder is configured with expected pattern"
+    (let [encoder (subject/simple-encoder-factory {:context (logger-context)})]
+      (is (= "%date %level %logger [%thread]: %msg%n" (.getPattern encoder))))))
+
+(deftest build-appender-console
+  (testing "appender is started by default"
+    (let [console-appender (subject/build-appender {:context (logger-context)
+                                                    :level :info
+                                                    :type :console})]
+      (is (= true (.isStarted console-appender)))))
+
+  (testing "appender is not started when false is given for :start?"
+    (let [console-appender (subject/build-appender {:context (logger-context)
+                                                    :level :info
+                                                    :start? false
+                                                    :type :console})]
+      (is (= false (.isStarted console-appender)))))
+
+  (testing "appender is configured with the given name when :name is given"
+    (let [name "appender-00"
+          console-appender (subject/build-appender {:context (logger-context)
+                                                    :level :info
+                                                    :name name
+                                                    :start? false
+                                                    :type :console})]
+      (is (= name (.getName console-appender)))))
+
+  (testing "appender is configured with the specified context"
+    (let [context (logger-context)
+          console-appender (subject/build-appender {:context context
+                                                    :level :info
+                                                    :start? false
+                                                    :type :console})]
+      (is (= context (.getContext console-appender)))))
+
+  (testing "appender is configured with threshold filter configured with given level"
+    (let [console-appender (subject/build-appender {:context (logger-context)
+                                                    :level :info
+                                                    :start? true
+                                                    :type :console})
+          threshold-filter* (-> console-appender .getCopyOfAttachedFiltersList seq first)]
+      (is (= true (.isStarted threshold-filter*)))
+      (is (= FilterReply/DENY (.getFilterChainDecision console-appender trace-event)))
+      (is (= FilterReply/DENY (.getFilterChainDecision console-appender debug-event)))
+      (is (= FilterReply/NEUTRAL (.getFilterChainDecision console-appender info-event)))
+      (is (= FilterReply/NEUTRAL (.getFilterChainDecision console-appender warn-event)))
+      (is (= FilterReply/NEUTRAL (.getFilterChainDecision console-appender error-event)))))
+
+  (testing "appender is configured with encoder generated by given :encoder-factory fn"
+    (let [encoder-factory-called (atom false)
+          encoder-instance (atom nil)
+          start? true
+          context (logger-context)
+          encoder-factory (fn [args]
+                            (reset! encoder-factory-called true)
+                            (reset! encoder-instance (subject/simple-encoder-factory args))
+                            (is (= start? (:start? args)))
+                            (is (= context (:context args)))
+                            @encoder-instance)
+          console-appender (subject/build-appender {:context context
+                                                    :encoder-factory encoder-factory
+                                                    :level :info
+                                                    :start? start?
+                                                    :type :console})]
+      (is (= true @encoder-factory-called))
+      (is (= @encoder-instance (.getEncoder console-appender))))))
+
+(deftest build-appender-file
+  (testing "appender is started by default"
+    (let [file-appender (subject/build-appender {:context (logger-context)
+                                                 :level :info
+                                                 :path "/tmp/file-logger-01"
+                                                 :type :file})]
+      (is (= true (.isStarted file-appender)))))
+
+  (testing "appender is not started when false is given for :start?"
+    (let [file-appender (subject/build-appender {:context (logger-context)
+                                                 :level :info
+                                                 :path "/tmp/file-logger-02"
+                                                 :start? false
+                                                 :type :file})]
+      (is (= false (.isStarted file-appender)))))
+
+  (testing "appender is configured with the given name when :name is given"
+    (let [name "appender-01"
+          file-appender (subject/build-appender {:context (logger-context)
+                                                 :level :info
+                                                 :name name
+                                                 :start? false
+                                                 :type :console})]
+      (is (= name (.getName file-appender)))))
+
+  (testing "appender is configured with the specified context"
+    (let [context (logger-context)
+          file-appender (subject/build-appender {:context context
+                                                 :level :info
+                                                 :path "/tmp/file-logger-03"
+                                                 :start? false
+                                                 :type :file})]
+      (is (= context (.getContext file-appender)))))
+
+  (testing "appender is configured with threshold filter configured with given level"
+    (let [file-appender (subject/build-appender {:context (logger-context)
+                                                 :level :info
+                                                 :path "/tmp/file-logger-04"
+                                                 :start? true
+                                                 :type :file})
+          threshold-filter* (-> file-appender .getCopyOfAttachedFiltersList seq first)]
+      (is (= true (.isStarted threshold-filter*)))
+      (is (= FilterReply/DENY (.getFilterChainDecision file-appender trace-event)))
+      (is (= FilterReply/DENY (.getFilterChainDecision file-appender debug-event)))
+      (is (= FilterReply/NEUTRAL (.getFilterChainDecision file-appender info-event)))
+      (is (= FilterReply/NEUTRAL (.getFilterChainDecision file-appender warn-event)))
+      (is (= FilterReply/NEUTRAL (.getFilterChainDecision file-appender error-event)))))
+
+  (testing "appender is configured with encoder generated by given :encoder-factory fn"
+    (let [encoder-factory-called (atom false)
+          encoder-instance (atom nil)
+          start? true
+          context (logger-context)
+          encoder-factory (fn [args]
+                            (reset! encoder-factory-called true)
+                            (reset! encoder-instance (subject/simple-encoder-factory args))
+                            (is (= start? (:start? args)))
+                            (is (= context (:context args)))
+                            @encoder-instance)
+          file-appender (subject/build-appender {:context context
+                                                 :encoder-factory encoder-factory
+                                                 :level :info
+                                                 :path "/tmp/file-logger-05"
+                                                 :start? start?
+                                                 :type :file})]
+      (is (= true @encoder-factory-called))
+      (is (= @encoder-instance (.getEncoder file-appender)))))
+
+  (testing "appender is configured with specified file path"
+    (let [path "/tmp/file-logger-6"
+          file-appender (subject/build-appender {:context (logger-context)
+                                                 :level :info
+                                                 :path path
+                                                 :start? true
+                                                 :type :file})]
+      (is (= path (.getFile file-appender))))))
+
+(deftest build-logger
+  (testing "logger is configured as additive when true is given for :additive?"
+    (let [logger (subject/build-logger {:additive? true
+                                        :appenders []
+                                        :name "logger-01"})]
+      (is (= true (.isAdditive logger)))))
+
+  (testing "logger defaults to not additive when no value is given for :additive?"
+    (let [logger (subject/build-logger {:appenders [] :name "logger-02"})]
+      (is (= false (.isAdditive logger)))))
+
+  (testing "logger is configured with the given context when one is given"
+    (let [context (logger-context)
+          logger (subject/build-logger {:appenders []
+                                        :context context
+                                        :name "logger-03"})]
+      (is (= context (.getLoggerContext logger)))))
+
+  (testing "logger generates new context when no :context is given"
+    (let [logger (subject/build-logger {:appenders []
+                                        :name "logger-04"})]
+      (is (= LoggerContext (class (.getLoggerContext logger))))))
+
+  (testing "logger has all appenders detached and stopped when true is given for :detach-and-stop-all-appenders?"
+    (let [appender-name "appender-02"
+          logger-name "logger-05"
+          initial-logger (subject/build-logger {:appenders [{:level :info
+                                                             :name appender-name
+                                                             :start? true
+                                                             :type :console}]
+                                                :name logger-name})
+          initial-appender (.getAppender initial-logger appender-name)]
+      (is (= true (.isStarted initial-appender)))
+      (let [logger (subject/build-logger {:appenders []
+                                          :detach-and-stop-all-appenders? true
+                                          :name logger-name})
+            appender (.getAppender logger appender-name)]
+        (is (= false (.isStarted initial-appender)))
+        (is (nil? appender)))))
+
+  (testing "by default, logger has all appenders detached and stopped when true is given for :detach-and-stop-all-appenders?"
+    (let [appender-name "appender-03"
+          logger-name "logger-06"
+          initial-logger (subject/build-logger {:appenders [{:level :info
+                                                             :name appender-name
+                                                             :start? true
+                                                             :type :console}]
+                                                :name logger-name})
+          initial-appender (.getAppender initial-logger appender-name)]
+      (is (= true (.isStarted initial-appender)))
+      (let [logger (subject/build-logger {:appenders []
+                                          :name logger-name})
+            appender (.getAppender logger appender-name)]
+        (is (= false (.isStarted initial-appender)))
+        (is (nil? appender)))))
+
+  (testing "logger does not have all appenders detached and stopped when false is given for :detach-and-stop-all-appenders?"
+    (let [appender-name "appender-04"
+          logger-name "logger-07"
+          initial-logger (subject/build-logger {:appenders [{:level :info
+                                                             :name appender-name
+                                                             :start? true
+                                                             :type :console}]
+                                                :name logger-name})
+          initial-appender (.getAppender initial-logger appender-name)]
+      (is (= true (.isStarted initial-appender)))
+      (let [logger (subject/build-logger {:appenders []
+                                          :detach-and-stop-all-appenders? false
+                                          :name logger-name})
+            appender (.getAppender logger appender-name)]
+        (is (= true (.isStarted initial-appender)))
+        (is (= initial-appender appender)))))
+
+  (testing "logger is configured with specified :level when keyword is given"
+    (let [logger (subject/build-logger {:appenders [] :level :info :name "logger-08"})]
+      (is (= Level/INFO (.getLevel logger)))))
+
+  (testing "logger is configured with specified :level when ch.qos.logback.classic.Level constant is given"
+    (let [logger (subject/build-logger {:appenders [] :level Level/INFO :name "logger-09"})]
+      (is (= Level/INFO (.getLevel logger)))))
+
+  (testing "logger assumes level of INFO when nil is given for :level"
+    (let [logger (subject/build-logger {:appenders [] :level nil :name "logger-10"})]
+      (is (= Level/INFO (.getLevel logger)))))
+
+  (testing "logger is configured with the specified :appenders"
+    (let [console-appender-name "appender-05"
+          file-appender-name "appender-06"
+          path "/tmp/logger-11"
+          context (logger-context)
+          logger (subject/build-logger {:additive? true
+                                        :appenders [{:level :info
+                                                     :name console-appender-name
+                                                     :start? true
+                                                     :type :console}
+                                                    {:level :info
+                                                     :name file-appender-name
+                                                     :path path
+                                                     :start? true
+                                                     :type :file}]
+                                        :context context
+                                        :name "logger-11"})
+          console-appender (.getAppender logger console-appender-name)
+          file-appender (.getAppender logger file-appender-name)]
+      (is (= console-appender-name (.getName console-appender)))
+      (is (= ConsoleAppender (class console-appender)))
+      (is (= context (.getContext console-appender)))
+      (is (= true (.isStarted console-appender)))
+      (is (= file-appender-name (.getName file-appender)))
+      (is (= FileAppender (class file-appender)))
+      (is (= path (.getFile file-appender)))
+      (is (= context (.getContext file-appender)))
+      (is (= true (.isStarted file-appender))))))
