8a392735d25232bc51e00384dffea9e5b10455e3
Danny Guinther <dannyguinther@gmail.com>
2019-04-25 22:05:21 -0400

    New crazy macro

--- src/hotlog/macro.clj
+++ src/hotlog/macro.clj
@@ -1,32 +1,35 @@
 (ns hotlog.macro
   (:require [clojure.string :as cstr]))
 
-;; Cursive-users
-(declare log!)
-
 ;; use org.clojure/tools.logging if available, otherwise fallback to clj console logger
 (defmacro defn-log!
-  "Define a log! fn in the calling namespace. Uses org.clojure/tools.logging if available, otherwise
-  falls back to logging messages to the console with println."
-  []
-  (try
-   (eval
-    `(do
-      (require 'clojure.tools.logging)
-      (defmacro ~'log! [& ~'args]
-        `(clojure.tools.logging/log ~@~'args))))
-   (catch Exception _
-     (eval
-      `(do
-        (defmacro ~'log! [~'level ~'ex & ~'more]
-          `(let [~'more# (if (instance? Throwable ~~'ex)
-                         (vec ~~'more)
-                         (into [~~'ex] ~~'more))]
-
-                         (println (str *ns*) (.toUpperCase (name ~~'level))
-                                  (clojure.string/join " " ~'more#))
-                         (when (instance? Throwable ~~'ex)
-                           (.printStackTrace ^Throwable ~~'ex))))
-        (~'log! :warn (str "clojure.tools.logging not found on classpath, "
-                           *ns*
-                           " logging to console.")))))))
+  "Define a log! fn identified by the given :logger-ns or the calling ns. Uses
+  org.clojure/tools.logging if available. If org.clojure/tools.logging is not available, then
+  logging behavior depends on the value given for the :fallback-to-println? option. When
+  :fallback-to-println? is true (the default), log messages are written to the console with println.
+  When :fallback-to-println? is false, log messages are swallowed."
+  ([] `(defn-log! {}))
+  ([{:keys [fallback-to-println? logger-ns] :or {fallback-to-println? true}}]
+   (try
+    (eval
+     `(do
+       (require 'clojure.tools.logging)
+       (let [logger-ns# (or (some-> ~logger-ns name) (str *ns*))]
+         (defn ~'log!
+            ([~'level ~'msg]
+             (~'log! ~'level nil ~'msg))
+            ([~'level ~'throwable ~'msg]
+             (clojure.tools.logging/log logger-ns# ~'level ~'throwable ~'msg))))))
+    (catch Exception _
+      (when fallback-to-println?
+        (eval
+         `(do
+           (let [logger-ns# (or (some-> ~logger-ns name) (str *ns*))]
+             (defn ~'log! [~'level ~'throwable & ~'more]
+               (let [~'more# (if (instance? Throwable ~'throwable)
+                               (vec ~'more)
+                               (into [~'throwable] ~'more))]
+                 (println logger-ns# (.toUpperCase (name ~'level)) (cstr/join " " ~'more#))
+                 (when (instance? Throwable ~'throwable)
+                   (.printStackTrace ^Throwable ~'throwable))))
+             (~'log! :warn (str "clojure.tools.logging not found on classpath, logging to console."))))))))))
